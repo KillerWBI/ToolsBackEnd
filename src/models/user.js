ae981086ea016bcee8797b2927f652a070d048b7
Oleksandr Baranov <baranov.sanyok1995@gmail.com>
2025-12-10 21:47:22 +0100

    Add public endpoint to fetch user info by ID

--- src/models/user.js
+++ src/models/user.js
@@ -1,21 +1,24 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
 
-
-
-const UserShema = new Schema(
+const userSchema = new Schema(
   {
     name: {
       type: String,
       required: true,
       trim: true,
+      minlength: 2,
+      maxlength: 32,
     },
     avatarUrl: {
       type: String,
-      required: true,
       trim: true,
-    }
+      default: "",
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  },
 );
-export const User = model('User', UserShema);
+
+export const User = model("User", userSchema);
 export default User;

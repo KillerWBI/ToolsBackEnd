399cf4a8049ac0f6b38f2a1eb77d09d35eac0c32
abrosenkov <abrosenkov91@gmail.com>
2025-12-12 00:54:24 +0100

    create updateTool route

--- src/routes/toolsRoutes.js
+++ src/routes/toolsRoutes.js
@@ -1,17 +1,26 @@
 import { celebrate } from 'celebrate';
 import { Router } from 'express';
 
-import { createTool, DeleteTool, getAllNotes, updateTool } from '../controllers/controlerTools.js';
+import {
+  createTool,
+  DeleteTool,
+  getAllNotes,
+  updateTool,
+} from '../controllers/controlerTools.js';
 import { authenticate } from '../middleware/authenticate.js';
-import { createToolSchema, DeleteToolShema, UpdateTollSchema } from '../validations/toolValidation.js';
+import {
+  createToolSchema,
+  DeleteToolShema,
+  updateToolSchema,
+} from '../validations/toolValidation.js';
 
 const router = Router();
 
 // защита всех маршрутов /Tool
-router.use("/Tool", authenticate);
+router.use('/Tool', authenticate);
 
-router.get("/Tool", getAllNotes);
-router.post("/Tool", celebrate(createToolSchema), createTool);
-router.patch("Tool/:id", celebrate(UpdateTollSchema), updateTool);
-router.delete("Tool/:id", celebrate(DeleteToolShema), DeleteTool);
+router.get('/Tool', getAllNotes);
+router.post('/Tool', celebrate(createToolSchema), createTool);
+router.patch('/tools/:toolId', celebrate(updateToolSchema), updateTool);
+router.delete('Tool/:id', celebrate(DeleteToolShema), DeleteTool);
 export default router;

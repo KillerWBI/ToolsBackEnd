b53ed4c3a6ca6da03fc6f709bc057d3a75d8cfd8
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-12-11 20:59:47 +0200

    /api/categories endpoint

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
-import { errors } from "celebrate";
-import cors from "cors";
-import "dotenv/config";
-import express from "express";
-import cookieParser from "cookie-parser";
+import { errors } from 'celebrate';
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+import cookieParser from 'cookie-parser';
 
-import { connectMongoDB } from "./db/connectMongoDB.js";
-import { errorHandler } from "./middleware/errorHandler.js";
-import { logger } from "./middleware/logger.js";
-import { notFoundHandler } from "./middleware/notFoundHandler.js";
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
 
-import ToolsRout from "./routes/ToolsRout.js";
-import UsersRout from "./routes/UsersRout.js";
-import bookingsRouter from "./routes/bookingsRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
+import ToolsRout from './routes/ToolsRout.js';
+import UsersRout from './routes/UsersRout.js';
+import bookingsRouter from './routes/bookingsRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import categoriesRouter from './routes/categoriesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -25,9 +26,10 @@ app.use(logger);
 
 // ===== Routes =====
 app.use(authRoutes); // авторизация и регистрация
-app.use(ToolsRout);  // инструменты
-app.use(UsersRout);  // пользователи
-app.use("/api/bookings", bookingsRouter); // бронирование
+app.use(ToolsRout); // инструменты
+app.use(UsersRout); // пользователи
+app.use('/api/bookings', bookingsRouter); // бронирование
+app.use('/api/categories', categoriesRouter); // категорії
 
 // ===== Handlers =====
 app.use(notFoundHandler);

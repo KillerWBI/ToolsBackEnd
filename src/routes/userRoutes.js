1cbdab03e45286f448a6117f24cbac4d63563b34
klymkov_ivan <klymkov90@gmail.com>
2025-12-11 17:43:33 +0200

    replace my code into usersController and UsersRout

--- src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { authenticate } from '../middleware/authenticate.js';
-import { getUser } from '../controllers/userController.js';
-
-const router = Router();
-
-router.get('/api/users/me', authenticate, getUser);
-
-export default router;

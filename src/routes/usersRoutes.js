75b1ddab506318d34b5607129f8a7ced97e21cb9
Yaremchuk-Sergey <bb01082014@gmail.com>
2025-12-14 19:33:45 +0200

    Добавлен маршрут для получения инструментов пользователя

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { getPublicUserById, getUser } from '../controllers/usersController.js';
 import { authenticate } from '../middleware/authenticate.js';
+import getUserTools from '../controllers/userToolsController.js';
 
 const router = Router();
 
 router.get('/me', authenticate, getUser);
 router.get('/:userId', getPublicUserById);
-
+router.get('/user/:userId', getUserTools);
 export default router;

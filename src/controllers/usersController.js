ead126891f35df0a2137126d069793d4419a8de2
Marina Prushak <87096097+huruzka@users.noreply.github.com>
2025-12-17 13:55:44 +0200

    added cloudinary

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
 const { isValidObjectId } = mongoose;
 
@@ -53,4 +54,21 @@ export const getUser = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+export const updateUserAvatar = async (req, res, next) => {
+  if (!req.file) {
+    next(createHttpError(400, 'No file'));
+    return;
+  }
+
+  const result = await saveFileToCloudinary(req.file.buffer);
+
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    { avatar: result.secure_url },
+    { new: true },
+  );
+
+  res.status(200).json({ url: user.avatar });
 };

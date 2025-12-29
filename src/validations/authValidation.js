422b0e15325eea8d1bdb3b7ebf9129762291e004
Stanislav Nemchenko <stas.buka@gmail.com>
2025-12-29 08:28:11 +0200

    add reset password functionality and update Swagger documentation

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -1,7 +1,7 @@
 import { Joi, Segments } from 'celebrate';
 
 export const registerUserSchema = {
-    [Segments.BODY]: Joi.object({
+  [Segments.BODY]: Joi.object({
     name: Joi.string().min(2).max(32).required(),
     email: Joi.string().email().max(64).required(),
     password: Joi.string().min(8).max(128).required(),
@@ -18,5 +18,12 @@ export const loginUserSchema = {
 export const requestResetEmailSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().email().required(),
+  }),
+};
+
+export const resetPasswordSchema = {
+  [Segments.BODY]: Joi.object({
+    token: Joi.string().required(),
+    password: Joi.string().min(8).max(128).required(),
   }),
 };
